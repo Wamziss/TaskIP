e23dafa6f49246ee93e8dd701ea9a6a9a02ddec6
Wamziss <hannahmwangi551@gmail.com>
2024-09-30 22:40:16 +0300

    Tailwind styles fixed

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // This ensures all relevant files are scanned
-  // content: ['./src/TaskIT_frontend/src/**/*.{html,js,jsx}'], // This ensures all relevant files are scanned
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
